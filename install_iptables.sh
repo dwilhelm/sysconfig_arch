48681e47d5a9798db32b604c46e0a9eda7fc341c
David Wilhelm <dave@jumbledpile.com>
2025-05-01 23:04:29 -0700

    Remove iptables

--- install_iptables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-iptables-restore < iptables/iptables.rules
-ip6tables-restore < iptables/ip6tables.rules
-iptables-save > /etc/iptables/iptables.rules
-ip6tables-save > /etc/iptables/ip6tables.rules
